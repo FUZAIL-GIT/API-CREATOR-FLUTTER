dc9fe7508e1cb1f455229032baaf016b8fe06f9f
FUZAIL-GIT <mohammadfuzailzaman@gmail.com>
2023-04-02 22:12:57 +0500

    auto package installation fixed

--- lib/src/common/services/network/internet_connectivity.dart
+++ lib/src/common/services/network/internet_connectivity.dart
@@ -1,16 +1,29 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:io';
 
 class InternetConnectivity {
+  // Future<bool> isInternetAvailable() async {
+  //   bool isConnected = false;
+  //   final connectivityResult = await (Connectivity().checkConnectivity());
+  //   if (connectivityResult == ConnectivityResult.mobile) {
+  //     // I am connected to a mobile network.
+  //     isConnected = true;
+  //   } else if (connectivityResult == ConnectivityResult.wifi) {
+  //     // I am connected to a wifi network.
+  //     isConnected = true;
+  //   }
+  //   return isConnected;
+  // }
+
   Future<bool> isInternetAvailable() async {
-    bool isConnected = false;
-    final connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile) {
-      // I am connected to a mobile network.
-      isConnected = true;
-    } else if (connectivityResult == ConnectivityResult.wifi) {
-      // I am connected to a wifi network.
-      isConnected = true;
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      return false;
     }
-    return isConnected;
+    return false;
   }
 }
